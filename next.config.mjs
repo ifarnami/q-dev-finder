369ba796049ab4bed9e330c53066bf709523632e
Farnam Faryar <farnam.faryar@gmail.com>
2024-10-24 02:22:34 +0330

    chore: :wrench: add base url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_API_URL: "https://api.github.com/users",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
