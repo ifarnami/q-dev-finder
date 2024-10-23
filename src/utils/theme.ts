7132bf50fc96c087bbf36c3d52ab279943ea002c
Farnam Faryar <farnam.faryar@gmail.com>
2024-10-23 22:54:37 +0330

    feat(MUI): :sparkles: add mui theme config

---
+++ src/utils/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: "#F6F8FF",
+    },
+    primary: {
+      main: "#0077FF",
+      light: "#60ABFF"
+    },
+  },
+});
